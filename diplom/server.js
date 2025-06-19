41ef37d8b3895494bc64842f0c670a48b04243b1
Khertek182 <i.hertek99@gmail.com>
2025-06-19 12:58:36 +0700

    Исправлено двойное подключение 'path'

--- diplom/server.js
+++ diplom/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Отдача статических файлов из папки public
-app.use(express.static('public'));
+
 
 // Пример простого маршрута для проверки работы сервера
 app.get('/', (req, res) => {
